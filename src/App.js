ce5ccb4b1e770aa26baf421635cb7d326eca0c0b
saiteja <tsaiteja7@gmail.com>
2020-05-10 01:10:17 +0530

    Adding search bar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { Component, useState, useEffect } from "react";
 
 const App = () => {
   const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("react");
 
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
   };
   useEffect(() => {
     fetchNews();
-  });
+  }, [searchQuery]);
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  };
   return (
     <div>
       <h2>News</h2>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+      // n is news // i is index
       {news.map((n, i) => (
         <p keys={i}>{n.title}</p>
       ))}
