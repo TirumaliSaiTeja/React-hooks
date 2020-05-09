cf4379a50ccb41fcb403a8aee56ddea255d334ec
saiteja <tsaiteja7@gmail.com>
2020-05-10 01:21:31 +0530

    controlling use effect behaviour

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import React, { Component, useState, useEffect } from "react";
 const App = () => {
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState("react");
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=react"
+  );
 
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
   };
   useEffect(() => {
     fetchNews();
-  }, [searchQuery]);
+  }, [url]);
   const handleChange = e => {
     setSearchQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   };
   return (
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
       </form>
