09f47943a3e9fd5114d7a8dfd2864e8ee4dcd853
saiteja <tsaiteja7@gmail.com>
2020-05-09 23:37:49 +0530

    creating News App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component, useState, useEffect } from "react";
 
 const App = () => {
+  const [news, setNews] = useState([]);
+
+  const fetchNews = () => {
+    fetch("http://hn.algolia.com/api/v1/search?query=react")
+      .then(result => result.json())
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
+  };
+  useEffect(() => {
+    fetchNews();
+  });
+  return (
+    <div>
+      <h2>News</h2>
+      {news.map((n, i) => (
+        <p keys={i}>{n.title}</p>
+      ))}
+    </div>
+  );
+};
+
+/*const App = () => {
   const [count, setCount] = useState(0);
 
   //when ever there is a state change, useEffect will change automatically
@@ -19,7 +41,7 @@ const App = () => {
       <button onClick={increment}>Clicked {count} times</button>
     </div>
   );
-};
+};*/
 /*class App extends Component {
   state = {
     count: 0
